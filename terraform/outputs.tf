dfe7f41a714d7c3df3c9bddff1115e367727bff4
mattmecca <matt.mecca15@gmail.com>
2025-09-06 22:00:26 -0500

    Changing INGRESS load balancer / controller to a non-beta version

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,7 +8,7 @@ output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
   value       = module.eks.cluster_endpoint
 }
-
+ 
 output "cluster_platform_version" {
   description = "Platform version for the cluster"
   value       = module.eks.cluster_platform_version
