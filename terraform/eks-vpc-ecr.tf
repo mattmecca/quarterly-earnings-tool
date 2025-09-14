afdcfed4068c19ec148d641051c09d6c0b5d8d99
mattmecca <matt.mecca15@gmail.com>
2025-09-14 16:35:39 -0500

    Again

--- terraform/eks-vpc-ecr.tf
+++ terraform/eks-vpc-ecr.tf
@@ -55,7 +55,7 @@ module "eks" {
   # Let the module handle KMS key creation and conflicts
   create_kms_key = false # true
   # kms_key_deletion_window_in_days = 7
-  cluster_encryption_config = []
+  # cluster_encryption_config = []
 
   # Person that creates cluster will also have ADMIN access
   enable_cluster_creator_admin_permissions = true
